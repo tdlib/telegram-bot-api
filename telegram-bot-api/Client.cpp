55f4863d1ab091416e931631a07e910fd6a635d4
Kazem Pooshgan <30390886+pooshgan@users.noreply.github.com>
2025-07-23 11:32:25 +0330

    Update Client.cpp

--- telegram-bot-api/Client.cpp
+++ telegram-bot-api/Client.cpp
@@ -7809,7 +7809,7 @@ void Client::on_update_authorization_state() {
                      td::make_unique<TdOnOkCallback>());
       }
 
-      send_request(make_object<td_api::addProxy>("127.0.0.1", 2080, true, make_object<td_api::proxyTypeSocks5>("", "")),
+      send_request(make_object<td_api::addProxy>("127.0.0.1", 48080, true, make_object<td_api::proxyTypeSocks5>("", "")),
                      td::make_unique<TdOnOkCallback>());
 
       auto request = make_object<td_api::setTdlibParameters>();
